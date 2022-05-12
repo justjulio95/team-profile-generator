44b3d2f339666cc6035ddbd93bf32b57d08ec6fd
Julio.S <just.julio95@gmail.com>
2022-05-12 11:30:07 -0400

    created and tested the employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,44 @@
 /* Following properties and methods
-    Name
-    id
-    Email
-    getName()
-    getId()
-    getEmail()
+    Name (CHECK)
+    id (CHECK)
+    Email (CHECK)
+    getName() (CHECK)
+    getId() (CHECK)
+    getEmail() (CHECK)
     getRole() - returns 'Employee'*/
+
+const Employee = require('../lib/Employee')
+
+test('creates an EMPLOYEE object', () => {
+    const employee = new Employee('Shawn', '22611', 'shawn@sao.com', 'employee')
+
+    expect(employee.name).toBe('Shawn');
+    expect(employee.id).toMatch(/[0-9]+/);
+    expect(employee.email).toEqual(expect.stringContaining("@"));
+    expect(employee.email).toEqual(expect.stringContaining(".com"));
+    expect(employee.role).toBe('employee')
+})
+
+test('returns the NAME value of EMPLOYEE', () => {
+    const employee = new Employee('Shawn');
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name))
+})
+
+test('returns the ID value of EMPLOYEE', () => {
+    const employee = new Employee('Shawn', '22611')
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id))
+})
+
+test('returns the EMAIL value of EMPLOYEE', () => {
+    const employee = new Employee('Shawn', '22611', 'shawn@sao.com')
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email))
+})
+
+test('returns the ROLE value of EMPLOYEE', () => {
+    const employee = new Employee('Shawn', '22611', 'shawn@sao.com', 'employee')
+
+    expect(employee.getRole()).toEqual(expect.stringContaining(employee.role))
+})
