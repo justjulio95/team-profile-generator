9c61a6fc880210650dd5ac1ef00148f7762266c6
Julio.S <just.julio95@gmail.com>
2022-05-12 11:49:40 -0400

    created and tested the Engineer object.

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,4 +1,30 @@
 /* Following properties and methods
-    github
-    getGithub()
-    getRole() - Overridden to return 'Engineer'*/
+    github (CHECK)
+    getGithub() (CHECK)
+    getRole() - Overridden to return 'Engineer' (CHECK)*/
+const Engineer = require('../lib/Engineer')
+
+test('create the ENGINEER object', () => {
+    const engineer = new Engineer('Troy', '12345', 'troy@sao.com', 'engineer', 'yort93')
+    // ensure inheritance works as expected
+    expect(engineer.name).toBe('Troy');
+    expect(engineer.id).toMatch(/[0-9]+/);
+    expect(engineer.email).toEqual(expect.stringContaining("@"));
+    expect(engineer.email).toEqual(expect.stringContaining(".com"));
+    expect(engineer.role).toBe('engineer')
+    // new test specific for engineer
+    expect(engineer.github).toBe('yort93')
+})
+
+// new test specific for engineer
+test('returns GITHUB value of ENGINEER', () => {
+    const engineer = new Engineer('Troy', '12345', 'troy@sao.com', 'engineer', 'yort93')
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github))
+})
+
+test('returns ROLE value of ENGINEER', () => {
+    const engineer = new Engineer('Troy', '12345', 'troy@sao.com', 'engineer', 'yort93')
+
+    expect(engineer.getRole()).toEqual(expect.stringContaining(engineer.role))
+})
