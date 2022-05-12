6212dc310a4e5ce53a1f6fd3249e44a41056a3a3
Julio.S <just.julio95@gmail.com>
2022-05-12 12:07:00 -0400

    created and tested the intern object

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,4 +1,30 @@
 /* Following properties and methods
-    school
+    school (CHECK)
     getSchool()
     getRole() - Overridden to return 'Intern'*/
+const Employee = require('../lib/Employee');
+const Intern = require('../lib/Intern')
+
+test('create the INTERN object', () => {
+    const intern = new Intern('Tate', '56290', 'tate@random.com', 'intern', 'UAB');
+    //ensure inheritance works as expected
+    expect(intern.name).toBe('Tate');
+    expect(intern.id).toMatch(/[0-9]+/);
+    expect(intern.email).toEqual(expect.stringContaining("@"));
+    expect(intern.email).toEqual(expect.stringContaining(".com"));
+    expect(intern.role).toBe('intern')
+    // new test specific for intern
+    expect(intern.school).toBe('UAB')
+})
+
+test('returns the SCHOOL value of INTERN', () => {
+    const intern = new Intern('Tate', '56290', 'tate@random.com', 'intern', 'UAB');
+
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school));
+})
+
+test('returns the ROLE value of INTERN', () => {
+    const intern = new Intern('Tate', '56290', 'tate@random.com', 'intern', 'UAB');
+
+    expect(intern.getRole()).toEqual(expect.stringContaining(intern.role))
+})
